acc22097402d9563dc6219674106454e9c74401c
adi221 <adimizrahi421@gmail.com>
2021-08-14 23:45:54 +0300

    Added storybook

--- src/RangeSlider/RangeSlider.stories.js
+++ src/RangeSlider/RangeSlider.stories.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import RangeSlider from './';
 import { centerDecorator } from '../../../utils/storybook/decorators';
-import styles from './RangeSlider.modules.scss';
 
 export default {
   title: 'User Inputs/RangeSlider',
@@ -10,17 +9,44 @@ export default {
 };
 
 export const Basic = () => {
-  const [value, setValue] = useState(50);
+  const [value1, setValue1] = useState(50);
+  const [value2, setValue2] = useState(0.5);
+  const [value3, setValue3] = useState(360);
+  const [value4, setValue4] = useState(0.2);
 
   return (
     <>
       <RangeSlider
         min={0}
         max={100}
-        value={value}
-        onChange={e => setValue(e.target.value)}
+        value={value1}
+        isDarkTheme
+        onChange={e => setValue1(e.target.value)}
       />
-      <p>Value: {value}</p>
+
+      <RangeSlider
+        min={0}
+        max={1}
+        value={value2}
+        onChange={e => setValue2(e.target.value)}
+      />
+
+      <RangeSlider
+        min={100}
+        max={500}
+        value={value3}
+        onChange={e => setValue3(e.target.value)}
+        disabled
+      />
+
+      <RangeSlider
+        min={0}
+        max={1}
+        value={value4}
+        onChange={e => setValue4(e.target.value)}
+        isDarkTheme
+        disabled
+      />
     </>
   );
 };
